dadf8f3dfcb8eca3b9befafd1b6882254f24a58c
huyng14 <huyqng1412@gmail.com>
2025-09-24 09:29:45 -0400

    9/22

---
+++ example_code/src/main.py
@@ -0,0 +1,16 @@
+""""Entry point for example code...
+"""
+def hello_kitty():
+    int_array = [1,'2','three', 4, 5]
+
+    for item in int_array:
+        print(f'{item}', end= ' ')
+if __name__=='__main__':
+    hello_kitty()
+
+
+
+
+
+
+
